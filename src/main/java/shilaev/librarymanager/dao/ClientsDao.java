c6e0a9df7d754cda710bf411979979554e7dde63
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-12-03 11:59:24 +0300

    add clients page + some refactoring

--- src/main/java/shilaev/librarymanager/dao/ClientsDao.java
+++ src/main/java/shilaev/librarymanager/dao/ClientsDao.java
@@ -18,11 +18,18 @@ public class ClientsDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    // READ
     public List<Client> selectAllClients() {
         String selectAllFromClients = "select * from clients;";
         return jdbcTemplate.query(selectAllFromClients, new ClientRowMapper());
     }
 
+    public Client selectClientById(int id) {
+        String selectClientById = "select * from clients where id = ?;";
+        return jdbcTemplate.queryForObject(selectClientById, new ClientRowMapper(), id);
+    }
+
+    // CREATE
     public void addNewClient(Client newClient) {
         String insertIntoClientsQuery = "insert into clients (" +
                 "last_name," +
