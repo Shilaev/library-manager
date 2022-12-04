2cba57dae77decc1611f9d11d6fb344d06e995be
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-12-04 22:52:33 +0300

    add validation

--- src/main/java/shilaev/librarymanager/models/Client.java
+++ src/main/java/shilaev/librarymanager/models/Client.java
@@ -1,21 +1,45 @@
 package shilaev.librarymanager.models;
 
 
-
 import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.sql.Date;
 
 public class Client {
+    @NotNull(message = "can't be null")
     private int id;
-    @Length(min = 1, max = 5, message = "1 - 5")
-    @NotNull
+
+    @Length(min = 1, max = 90, message = "Length must be between 1 and 90")
+    @NotNull(message = "can't be null")
+    @NotEmpty(message = "can't be empty")
     private String lastName;
+
+    @Length(min = 1, max = 90, message = "Length must be between 1 and 90")
+    @NotNull(message = "can't be null")
+    @NotEmpty(message = "can't be empty")
     private String firstName;
+
+    @Length(min = 1, max = 90, message = "Length must be between 1 and 90")
+    @NotNull(message = "can't be null")
+    @NotEmpty(message = "can't be empty")
     private String patronymicName;
+
     private Date birthday;
+
+    @NotNull(message = "can't be null")
+    @NotEmpty(message = "can't be empty")
+    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$",
+            message = "phone number format must be: +1(234)568-8910")
     private String phone;
+
+    @Email(message = "check correction of email adres")
+    @NotNull(message = "can't be null")
+    @NotEmpty(message = "can't be empty")
     private String email;
 
     public Client(int id, String lastName, String firstName, String patronymicName, Date birthday, String phone, String email) {
